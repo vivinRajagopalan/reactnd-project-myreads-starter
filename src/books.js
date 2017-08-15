94e20a6b37866b0fad9ce2357820c71f657fabc3
vivinRajagopalan <connectingvivin@gmail.com>
2017-08-15 02:23:31 -0600

    on successful update , update the books

--- src/books.js
+++ src/books.js
@@ -11,25 +11,20 @@ import PropTypes from 'prop-types';
  		book: PropTypes.object.isRequired
  	}
 
-
  	onChangeBookShelf = (e) => {
 
  		BooksAPI.update(this.props.book, e.target.value).then((book)=> {
+        BooksAPI.getAll().then((books) =>{
+          this.props.updatebooks(books);
+      })
  		})
-
- 		BooksAPI.getAll().then((books) =>{
-			this.props.updatebooks(books);
-		})
-
-
-
- 	}
+  }
 	render(){
 		return(
 		  <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + this.props.book.imageLinks.smallThumbnail + ')' }}>
-                       <SelectBookCategory changeBookShelf={this.onChangeBookShelf}></SelectBookCategory>
+                       <SelectBookCategory changeBookShelf={this.onChangeBookShelf} shelf={this.props.book.shelf}></SelectBookCategory>
                     </div>
                 </div>    
                 <div className="book-title">{this.props.book.title}</div>
